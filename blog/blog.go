8944250f272c7dcc7e8ff101e0124fa633145b29
Enin Kaduk <eninkadukk@gmail.com>
2024-07-17 21:31:46 +0200

    update: blog
    Signed-off-by: Enin Kaduk <eninkadukk@gmail.com>

---
+++ blog/blog.go
@@ -0,0 +1,50 @@
+package blogposts
+
+// import "testing/fstest"
+import "io/fs"
+import "io"
+import "bufio"
+
+type Post struct {
+	Title string
+	Description string
+}
+
+func NewPostsFromFS(fileSystem fs.FS) ([]Post, error) {
+	dir, err := fs.ReadDir(fileSystem, ".")
+	if err != nil {
+		return nil, err
+	}
+	var posts []Post
+	for _, f := range dir {
+		post, err := getPost(fileSystem, f.Name())
+		if err != nil {
+			return nil, err //todo: needs clarification, should we totally fail if one file fails? or just ignore?
+		}
+		posts = append(posts, post)
+	}
+	return posts, nil
+}
+
+func getPost(fileSystem fs.FS, fileName string) (Post, error) {
+	postFile, err := fileSystem.Open(fileName)
+	if err != nil {
+		return Post{}, err
+	}
+	defer postFile.Close()
+	return newPost(postFile)
+}
+
+func newPost(postFile io.Reader) (Post, error) {
+	scanner := bufio.NewScanner(postFile)
+
+	readLine := func() string {
+		scanner.Scan()
+		return scanner.Text()
+	}
+
+	title := readLine()[7:]
+	description := readLine()[13:]
+
+	return Post{Title: title, Description: description}, nil
+}
---
+++ blog/go.mod
@@ -0,0 +1,3 @@
+module github.com/blueprismo/blogposts
+
+go 1.22.1
